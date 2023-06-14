f5be654c98a36cc77be03f8af4a0c437ba23c25f
Otizam <106086154+Otizam@users.noreply.github.com>
2023-06-14 22:34:55 +0100

    Add files via upload

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,69 +1,95 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
-import { AiOutlineBars, AiOutlineClose,  AiOutlineGithub, AiTwotoneMail,} from 'react-icons/ai';
-import {FaLinkedin, } from "react-icons/fa"
-import {BsGithub, BsPersonLinesFill} from "react-icons/bs"
-
+import { AiOutlineBars, AiOutlineClose, AiOutlineGithub, AiTwotoneMail } from 'react-icons/ai';
+import { FaLinkedin } from "react-icons/fa"
+import { BsGithub, BsPersonLinesFill } from "react-icons/bs";
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const[shadow, setShadow] =useState(false);
-   
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('#ecf0f3')
+  const [linkColor, setLinkColor] = useState('#1f2937')
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      router.asPath === '/property' ||
+      router.asPath === '/crypto' ||
+      router.asPath === '/netflix' ||
+      router.asPath === '/twitch'
+    ) {
+      setNavBg('transparent')
+      setLinkColor('#ecf0f3')
+    } else {
+      setNavBg('#ecf0f3')
+      setLinkColor('#1f2937')
+    }
+  }, [router])
+
   const handleNav = () => {
-    setNav(!nav)
+    setNav(!nav);
   };
 
-  useEffect( ()=> {
-   const handleShadow = () => {
-    if (window.scrollY >=90) {
-      setShadow(true)
-    } else {
-      setShadow(false)
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+    return () => {
+      window.removeEventListener("scroll", handleShadow);
     }
-   }
-   window.addEventListener("scroll", handleShadow)
-  }, [])
-      /* Navbar */
-  return(
-    <div className={ shadow
+  }, []);
+
+  return (
+    <div
+      style={{ background: navBg }}
+      className={
+        shadow
           ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
-          : 'fixed w-full h-20 z-[100]'}>
+          : 'fixed w-full h-20 z-[100]'
+      }
+    >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-  
-        <Image 
-              src="/../public/assets/navLogo.png" 
-              alt='/'
-              width="125"
-              height="50"
-              />
-       
-        <ul className="hidden md:flex">
-          <li className='ml-10 text-sm uppercase hover:border-b'>
-            <Link href='/'>Home</Link>
-          </li>
-          <li className='ml-10 text-sm uppercase hover:border-b'>
-            <Link href='/#about'>About</Link>
-          </li>
-          <li className='ml-10 text-sm uppercase hover:border-b'>
-            <Link href='/#skills'>Skills</Link>
-          </li>
-          <li className='ml-10 text-sm uppercase hover:border-b'>
-            <Link href='/#projects'>Projects</Link>
-          </li>
-          <li className='ml-10 text-sm uppercase hover:border-b'>
-            <Link href='/#contact'>Contact</Link>
-          </li>
-        </ul>  
-        <div onClick={handleNav}  className="md:hidden">
-          <AiOutlineBars  size={25}/>
+        <Link href='/'>
+          <Image
+            src="/../public/assets/navLogo.png"
+            alt='/'
+            width="125"
+            height="50"
+          />
+        </Link>
+        <div>
+          <ul style={{ color: linkColor }} className='hidden md:flex'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#about'>About</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#skills'>Skills</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#projects'>Projects</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#contact'>Contact</Link>
+            </li>
+          </ul>
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineBars size={25} />
+          </div>
         </div>
       </div>
 
-  
-  {/* Mobile Menu */}
-  {/* Overlay */}
-      
+      {/* Mobile Menu */}
+      {/* Overlay */}
       <div className={
           nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
         }
@@ -98,19 +124,19 @@ const Navbar = () => {
 
          <div className="py-4 flex flex-col">
             <ul className="uppercase">
-            <li onClick={()=> setNav(fasle)} className='py-4 text-sm'>
+            <li onClick={()=> setNav(false)} className='py-4 text-sm'>
                   <Link href='/'>Home</Link>
             </li>
-            <li onClick={()=> setNav(fasle)} className='py-4 text-sm'>
+            <li onClick={()=> setNav(false)} className='py-4 text-sm'>
                   <Link href='/#about'>About</Link>
             </li>
-            <li onClick={()=> setNav(fasle)} className='py-4 text-sm'>
+            <li onClick={()=> setNav(false)} className='py-4 text-sm'>
                   <Link href='/#skills'>Skills</Link>
             </li>
-            <li onClick={()=> setNav(fasle)} className='py-4 text-sm'>
+            <li onClick={()=> setNav(false)} className='py-4 text-sm'>
                   <Link href='/#projects'>Projects</Link>
             </li>
-            <li onClick={()=> setNav(fasle)} className='py-4 text-sm'>
+            <li onClick={()=> setNav(false)} className='py-4 text-sm'>
                   <Link href='/#contact'>Contact</Link>
             </li>
             </ul>
